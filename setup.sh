0b4a2d424c48fb8121b9d2c9e7717c50b78cda22
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-10-21 10:47:19 +0100

    add init update

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ sudo mv etc\ init.d/* /etc/init.d/
 mv home/* /home/pi
 mv LCD/* ~/Adafruit-Raspberry-Pi-Python-Code/Adafruit_CharLCDPlate
 sudo mv usr\ local\ bin/* /usr/local/bin/
+sudo update-rc.d lcd_start.sh defaults
