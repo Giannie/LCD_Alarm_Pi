d3f2bf62ed18649aa6cda55b88a79166224ab7b3
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-10-21 12:35:34 +0100

    edited setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
-sudo mv etc\ init.d/* /etc/init.d/
-mv home/* /home/pi
-mv LCD/* ~/Adafruit-Raspberry-Pi-Python-Code/Adafruit_CharLCDPlate
-sudo mv usr\ local\ bin/* /usr/local/bin/
-sudo update-rc.d lcd_start.sh defaults
+if [ "$(pwd)" != "/home/pi" ]; then
+        echo "Run script from home directory"
+        exit 0
+fi
+if [ -z "$(crontab -l | grep Alarm)" ]; then
+	line="# 0 7 * * * sh /usr/local/bin/alarmcron.sh > ~/cron.txt # Alarm"
+	(crontab -l; echo "$line" ) | crontab -
+fi
+sudo apt-get install mpd mpc python-pip
+sudo pip install python-crontab
+sudo pip install wiringpi
+git clone https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code
+git clone https://github.com/dmcg/raspberry-strogonanoff
+mkdir logs
+mkdir logs/lcd
+mkdir logs/cron
+
+sudo mv /home/pi/LCD_Alarm_Pi/etc\ init.d/* /etc/init.d/
+mv /home/pi/LCD_Alarm_Pi/home/* /home/pi
+mv /home/pi/LCD_Alarm_Pi/LCD/* ~/Adafruit-Raspberry-Pi-Python-Code/Adafruit_CharLCDPlate
+sudo mv /home/pi/LCD_Alarm_Pi/usr\ local\ bin/* /usr/local/bin/
+sudo mv /home/pi/LCD_Alarm_Pi/etc/* /etc
+sudo update-rc.d lcd_start.sh defaults 100
+
+sudo sh -c "echo \"//192.168.0.18/devices /mnt/raspbmc cifs credentials=/home/pi/.raspbmc_auth,nofail\" >> /etc/fstab"
