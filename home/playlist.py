aee5a1c50ffc57bb92ac063b9f5504a878f838c5
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-10-21 13:36:58 +0100

    remove folder

--- home/playlist.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import subprocess
-
-f = open("/home/pi/Alarm Playlist.m3u",'r')
-g = open('/home/pi/AlarmPlaylist.m3u','w')
-for line in f:
-    g.write(line.replace('/Users/Giancarlo/Music/iTunes/iTunes Music/', ''))
-g.close()
-f.close()
