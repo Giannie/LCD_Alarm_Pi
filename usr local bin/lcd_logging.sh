42948f9f8a15381dacc93298ed40d148ec201a90
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-11-02 12:51:56 +0000

    permissions

--- usr local bin/lcd_logging.sh
+++ usr local bin/lcd_logging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cat /home/pi/logs/lcd/lcd_error.log >> /home/pi/logs/lcd/lcd_error.log1
-sudo cat /home/pi/logs/lcd/out_lcd.log >> /home/pi/logs/lcd/out_lcd.log1
+sudo cat /home/pi/logs/lcd/lcd_error.log > /home/pi/logs/lcd/lcd_error.log1
+sudo cat /home/pi/logs/lcd/out_lcd.log > /home/pi/logs/lcd/out_lcd.log1
 sudo service lcd_start.sh restart
