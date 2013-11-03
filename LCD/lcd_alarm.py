e195da87d4a927df05e666d1adc8aff7eaea58bc
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-11-03 20:58:11 +0000

    backlight settings

--- LCD/lcd_alarm.py
+++ LCD/lcd_alarm.py
@@ -268,6 +268,7 @@ while True:
                                                         elif setting == 1:
                                                             subprocess.call("reboot")
                                                     elif n == select and setting_confirm == 2:
+                                                        lcd_string_prev = ''
                                                         stay = False
                                                         stay_again = False
                                                         break
