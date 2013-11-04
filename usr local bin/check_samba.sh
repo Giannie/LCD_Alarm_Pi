c755fbf953b9d2de2478b2dbca483513631415a0
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-11-04 18:12:04 +0000

    fix samba check

--- usr local bin/check_samba.sh
+++ usr local bin/check_samba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z \"$(ls /mnt/raspbmc/02611632-508e-42a3-9d69-566bdca87be8/)\" ]
+if [ -z "$(ls /mnt/raspbmc/02611632-508e-42a3-9d69-566bdca87be8/)" ]
 then 
 	sudo mount -a
 fi
