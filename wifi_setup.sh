84eb502ced44b559f219e922753e78ad594d576e
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-11-05 09:17:40 +0000

    fixed wifi reference

--- wifi_setup.sh
+++ wifi_setup.sh
@@ -5,7 +5,7 @@ sudo chown root:root /etc/network/interfaces
 sudo chmod 644 /etc/network/interfaces
 
 sudo cp LCD_Alarm_Pi/wifi/supplicant.txt /etc/wpa_supplicant/wpa_supplicant.conf
-sudo chown root:root /etc/wpa_supplicant/interfaces
+sudo chown root:root /etc/wpa_supplicant/wpa_supplicant.conf
 sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo "Add SSID and Password to /etc/wpa_supplicant/wpa_supplicant.conf"
