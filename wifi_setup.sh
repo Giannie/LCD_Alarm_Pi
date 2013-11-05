5e6be1b4d2ad944f33bd2bc352d76ff570d0134f
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-11-05 09:17:02 +0000

    fixed wifi reference

--- wifi_setup.sh
+++ wifi_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo cp LCD_Alarm_Pi/wifi/interfaces /etc/network/interfaces
+sudo cp LCD_Alarm_Pi/wifi/interfaces.txt /etc/network/interfaces
 sudo chown root:root /etc/network/interfaces
 sudo chmod 644 /etc/network/interfaces
 
-sudo cp LCD_Alarm_Pi/wifi/supplicant /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp LCD_Alarm_Pi/wifi/supplicant.txt /etc/wpa_supplicant/wpa_supplicant.conf
 sudo chown root:root /etc/wpa_supplicant/interfaces
 sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
 
