4b577be0008a30e905986afad59bf8e71183c5e7
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2013-11-06 09:20:08 +0000

    test

--- scripts/BBC_playlist.py
+++ scripts/BBC_playlist.py
@@ -24,10 +24,11 @@ def fetch_stream(url):
 
 
 #file = open (plsdir + "bbc.pls", "w+") #opens the file, +w creates it if it doesn't exist
-for (name,url) in stations.iteritems():
-    file = open(plsdir + name + '.m3u','w+')
-    file.write('#EXTM3U\n')
-    file.write('\n')
-    file.write('#EXTINF:-1, %s\n' % name)
-    file.write(fetch_stream(url) + '\n')
-    file.close()
+def generate():
+    for (name,url) in stations.iteritems():
+        file = open(plsdir + name + '.m3u','w+')
+        file.write('#EXTM3U\n')
+        file.write('\n')
+        file.write('#EXTINF:-1, %s\n' % name)
+        file.write(fetch_stream(url) + '\n')
+        file.close()
