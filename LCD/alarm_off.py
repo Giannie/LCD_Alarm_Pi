fa3ee4a5fe5187110ef6e30176abf23b00fc5559
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2014-05-02 11:45:04 +0100

    fixed alarm off script

--- LCD/alarm_off.py
+++ LCD/alarm_off.py
@@ -1,4 +1,4 @@
 from alarm_time import get_time, set_alarm
 
-new_setting = alarm_time.get_time()
-alarm_time.set_alarm(new_setting[0], new_setting[1], new_setting[2])
+new_setting = get_time()
+set_alarm(new_setting[0], new_setting[1], new_setting[2])
